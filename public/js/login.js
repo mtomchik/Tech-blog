7019984be5c918b3847ec73be7f57b367d249dce
Matt Tomchik <matthew.tomchik@yahoo.com>
2023-02-22 13:44:58 -0700

    Revert "final attempt before turning in"
    This reverts commit b6913f5d3f0e03a881dd5df0f6a77543edd083c6.

--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,31 @@
-//*
-//* This is the login helper code
-//*
-
-// event handler for the existing user login form
-const loginFormHandler = async (event) => {
-  // The preventDefault() method of the Event interface
-  // tells the user agent that if the event does not get explicitly handled,
-  // its default action should not be taken as it normally would be.
+async function loginFormHandler(event) {
   event.preventDefault();
-  // this code below will remove any leading or trailing spaces
-  // in the 'email' and 'passowrd' fields
-  const email = document.querySelector('#email-login').value.trim();
+
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
-    // the 'fetch' send User login info to the login API
+  if (username && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to log in.');
+      alert(response.statusText);
     }
   }
-};
+}
 
-// add the 'Event Listeners' to the page
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+
